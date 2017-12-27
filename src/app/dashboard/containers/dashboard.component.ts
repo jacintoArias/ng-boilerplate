9d43ccafe7203e839d0df54b32a1403c411a4331
Jacinto Arias <arias.jacinto@gmail.com>
2017-12-27 20:00:03 +0100

    Material layout as dumb components

--- src/app/dashboard/containers/dashboard.component.ts
+++ src/app/dashboard/containers/dashboard.component.ts
@@ -4,20 +4,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-dashboard',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-
-    <mat-sidenav-container fullscreen>
-      <mat-sidenav mode="push" [opened]="true">
-        <mat-nav-list>
-          <app-sidenav-item routerLink="/" icon="home" hint="Home" ></app-sidenav-item>
-          <app-sidenav-item routerLink="/" icon="lock" hint="Log Out" ></app-sidenav-item>
-        </mat-nav-list>
-      </mat-sidenav>
-      <mat-sidenav-content>
-        <mat-toolbar color="primary">
-        </mat-toolbar>
-        <router-outlet></router-outlet>
-      </mat-sidenav-content>
-    </mat-sidenav-container>
+    <app-layout>
+      <app-sidenav>
+        <app-sidenav-item routerLink="/" icon="home" hint="Home" ></app-sidenav-item>
+        <app-sidenav-item routerLink="/" icon="lock" hint="Log Out" ></app-sidenav-item>
+      </app-sidenav>
+      <app-toolbar></app-toolbar>
+      <router-outlet></router-outlet>
+    </app-layout>
 `,
   styles: []
 })
