3c45fd64c89a1f9a37b3804694e30158b0237846
Jacinto Arias <arias.jacinto@gmail.com>
2017-12-28 15:28:58 +0100

    Removed toolbar encapsulation to fit material restrictions

--- src/app/dashboard/containers/dashboard.component.ts
+++ src/app/dashboard/containers/dashboard.component.ts
@@ -1,29 +1,43 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
 import { AuthService } from '@app/auth';
 
 @Component({
   selector: 'app-dashboard',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <app-layout>
-      <app-sidenav>
-        <app-sidenav-item routerLink="/" icon="home" hint="Home" ></app-sidenav-item>
-        <app-sidenav-item (click)="logout()" icon="lock" hint="Log Out" ></app-sidenav-item>
-      </app-sidenav>
-      <app-toolbar></app-toolbar>
-      <router-outlet></router-outlet>
-    </app-layout>
+    <mat-sidenav-container fullscreen>
+
+      <mat-sidenav mode="side" [opened]="showSidenav$ | async">
+        <mat-nav-list>
+          <app-sidenav-item routerLink="/" icon="home" hint="Home" ></app-sidenav-item>
+          <app-sidenav-item (click)="logout()" icon="lock" hint="Log Out" ></app-sidenav-item>
+        </mat-nav-list>
+      </mat-sidenav>
+
+      <mat-sidenav-content>
+        <app-toolbar>
+          
+        </app-toolbar>
+        <router-outlet></router-outlet>
+      </mat-sidenav-content>
+
+    </mat-sidenav-container>
 `,
   styles: []
 })
 export class DashboardComponent {
 
+  showSidenav$: Observable<boolean>;
+
   constructor(
     private authService: AuthService,
   ) {
+    this.showSidenav$ = Observable.fromPromise(Promise.resolve(true));
   }
 
-  openSidenav() {
+  toggleSidenav() {
     // this.store.dispatch(new layout.OpenSidenav());
   }
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,9 +5,7 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './containers/dashboard.component';
-import { LayoutComponent } from './components/layout.component';
 import { HomeComponent } from './containers/home.component';
-import { SidenavComponent } from './components/sidenav.component';
 import { SidenavItemComponent } from './components/sidenav-item.component';
 import { ToolbarComponent } from './components/toolbar.component';
 
@@ -18,10 +16,8 @@ import { ToolbarComponent } from './components/toolbar.component';
     DashboardRoutingModule,
   ],
   declarations: [
-    LayoutComponent,
     DashboardComponent,
     HomeComponent,
-    SidenavComponent,
     SidenavItemComponent,
     ToolbarComponent,
   ]
